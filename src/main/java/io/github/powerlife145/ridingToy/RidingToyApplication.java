bf57155633c1e81f410009f4f075fdd41859f03d
powerlife145 <powerlife145@gmail.com>
2025-05-14 20:47:54 +0900

    chore: first commit

---
+++ src/main/java/io/github/powerlife145/ridingToy/RidingToyApplication.java
@@ -0,0 +1,13 @@
+package io.github.powerlife145.ridingToy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidingToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RidingToyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/powerlife145/ridingToy/RidingToyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.powerlife145.ridingToy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidingToyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
